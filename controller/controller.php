ea4148b30792da64515f587e9639e402c5d24b32
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-10 23:51:12 -0500

    ajustes menores al controller

--- controller/controller.php
+++ controller/controller.php
@@ -7,13 +7,10 @@ $result_set = $sentencia->get_personas(); // devuelve un conjunto de arreglos, c
 $data = Array();
 foreach ($result_set as $row) {
     //echo "{'id':'".$row["id_persona"]."'}";
-    //print_r($row); // se imprime un array con propiedades como indices
-    //print("<br>");
+    // print_r($row); // se imprime un array con propiedades como indices
+    // print("<br>");
     array_push($data, array("id_persona" => $row['id_persona'], "nombres" => $row['nombres'], "apellidos" => $row['apellidos'], "genero" => $row['genero']));
     $json = json_encode($data); //formateado como json
 }
-//print_r($json);
-//print("<br>");
 echo $json;
-
 ?>
