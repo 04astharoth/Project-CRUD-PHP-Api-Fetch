39a20990bd85e9639df49cfed8cff7e8e0d94165
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-10 23:00:22 -0500

    construccion del controlador

--- controller/controller.php
+++ controller/controller.php
@@ -3,11 +3,17 @@ require_once "../config/dbconexion.php";
 require_once "../model/model.php";
 
 $sentencia = new consultas();
-$result_set = $sentencia->get_personas(); // devuelve un array con el conjunto de datos de la db
-
+$result_set = $sentencia->get_personas(); // devuelve un conjunto de arreglos, cada arreglo es una fila de la tabla de la db
+$data = Array();
 foreach ($result_set as $row) {
     //echo "{'id':'".$row["id_persona"]."'}";
-    print_r($row);
-    print("<br>");
+    //print_r($row); // se imprime un array con propiedades como indices
+    //print("<br>");
+    array_push($data, array("id_persona" => $row['id_persona'], "nombres" => $row['nombres'], "apellidos" => $row['apellidos'], "genero" => $row['genero']));
+    $json = json_encode($data); //formateado como json
 }
+//print_r($json);
+//print("<br>");
+echo $json;
+
 ?>
