2bf9f21114f262797a061922b831f8ce727da898
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-16 20:12:45 -0500

    Adicion botones actualizar y eliminar registro

--- js/fetch.js
+++ js/fetch.js
@@ -3,17 +3,22 @@ fetch("../../controller/controller.php")
 	//.then(text => console.log(text))
 	.then((response) =>
 		response.map((data) => {
-			console.log(data);
+			//console.log("Success", data);
 			let tbody = document.querySelector("#data");
 			let row = document.createElement("tr");
 			// obteniendo data de la response
 			for (const [key, value] of Object.entries(data)) {
-                let elemento = key == "id_persona" ? "th" : "td";
+				let elemento = key == "id_persona" ? "th" : "td";
 				let td = document.createElement(elemento);
 				//element.innerHTML = `${key}: ${value}`
 				td.innerHTML = `${value}`;
 				row.append(td);
 			}
+			// adicionando los botones update y delete
+			let colAcciones = document.createElement("td");
+			colAcciones.innerHTML =
+				'<button type="button" class="btn btn-success"><i class="bi bi-arrow-repeat"></i></button><button type="button" class="btn btn-danger"><i class="bi bi-trash"></i></button>';
+			row.appendChild(colAcciones);
 			tbody.append(row);
 			//   let resultDiv = document.querySelector("#result")
 			//   let element = document.createElement('p');
