6bbf0afa7a43f427a783c860a1c2305f56c42e9c
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-16 20:14:42 -0500

    adicion de funcion insert persona

--- model/model.php
+++ model/model.php
@@ -9,6 +9,18 @@ class consultas extends dbconexion
         $sql->execute();
         return $array = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
-}
 
-?>
+    public function insert_persona($nombres,$apellidos,$genero)
+    {
+        $conectar = dbconexion::conexion();
+        $sql = $conectar->prepare("INSERT INTO persona (nombres,apellidos,genero) VALUES (?,?,?)");
+        $sql = bindValue(1, $nombres);
+        $sql = bindValue(2, $apellidos);
+        $sql = bindValue(3, $genero);
+
+        if ($sql->execute()) {
+            $resultado = self::get_personas();
+            return $resultado;
+        } 
+    }
+}
