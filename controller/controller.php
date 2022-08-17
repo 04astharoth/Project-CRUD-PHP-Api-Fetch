6fd6097c0773d2ad1e189f25f1deb92b9deb028c
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-16 20:13:34 -0500

    ajustes generales al controller, comportamiento de consulta de registros

--- controller/controller.php
+++ controller/controller.php
@@ -3,8 +3,9 @@ require_once "../config/dbconexion.php";
 require_once "../model/model.php";
 
 $sentencia = new consultas();
+
 $result_set = $sentencia->get_personas(); // devuelve un conjunto de arreglos, cada arreglo es una fila de la tabla de la db
-$data = Array();
+$data = array();
 foreach ($result_set as $row) {
     //echo "{'id':'".$row["id_persona"]."'}";
     // print_r($row); // se imprime un array con propiedades como indices
@@ -13,4 +14,17 @@ foreach ($result_set as $row) {
     $json = json_encode($data); //formateado como json
 }
 echo $json;
-?>
+
+// switch ($_GET['opc']) {
+//     case 'guardar':
+
+
+//         echo '<script>';
+//         echo 'console.log("Funciona el guardar")';
+//         echo '</script>';
+//         break;
+
+//     default:
+//         # code...
+//         break;
+// }
