6a0981d80cc1f66f6cdde1cfa5469069b15aef93
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-16 22:13:29 -0500

    ajuste al controller para atender peticions por medio de opciones

--- controller/controller.php
+++ controller/controller.php
@@ -4,27 +4,29 @@ require_once "../model/model.php";
 
 $sentencia = new consultas();
 
-$result_set = $sentencia->get_personas(); // devuelve un conjunto de arreglos, cada arreglo es una fila de la tabla de la db
-$data = array();
-foreach ($result_set as $row) {
-    //echo "{'id':'".$row["id_persona"]."'}";
-    // print_r($row); // se imprime un array con propiedades como indices
-    // print("<br>");
-    array_push($data, array("id_persona" => $row['id_persona'], "nombres" => $row['nombres'], "apellidos" => $row['apellidos'], "genero" => $row['genero']));
-    $json = json_encode($data); //formateado como json
+switch ($_GET["op"]) {
+    case 'listar':
+        $result_set = $sentencia->get_personas(); // devuelve un conjunto de arreglos, cada arreglo es una fila de la tabla de la db
+        $data = array();
+        foreach ($result_set as $row) {
+            //echo "{'id':'".$row["id_persona"]."'}";
+            // print_r($row); // se imprime un array con propiedades como indices
+            // print("<br>");
+            array_push($data, array("id_persona" => $row['id_persona'], "nombres" => $row['nombres'], "apellidos" => $row['apellidos'], "genero" => $row['genero']));
+            $json = json_encode($data); //formateado como json
+        }
+        echo $json;
+        break;
+
+    case 'guardar':
+        $nombres = $_POST["nombres"];
+        $apellidos = $_POST["apellidos"];
+        $genero = $_POST["genero"];
+        $insercion = $sentencia->insert_persona($nombres, $apellidos, $genero);
+        echo json_encode($insercion);
+        break;
+
+    default:
+        # code...
+        break;
 }
-echo $json;
-
-// switch ($_GET['opc']) {
-//     case 'guardar':
-
-
-//         echo '<script>';
-//         echo 'console.log("Funciona el guardar")';
-//         echo '</script>';
-//         break;
-
-//     default:
-//         # code...
-//         break;
-// }
