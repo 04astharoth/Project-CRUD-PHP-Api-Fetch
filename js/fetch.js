df3a89f197b8e4c93072d2a2b25f5093ab14ff77
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-19 16:18:07 -0500

    recontruccion de script fetch, se cambia la manera en como se van a mostrar los datos

--- js/fetch.js
+++ js/fetch.js
@@ -1,26 +1,22 @@
 fetch("../../controller/controller.php?op=listar")
 	.then((response) => response.json())
 	//.then(text => console.log(text))
-	.then((response) =>
-		response.map((data) => {
-			//console.log("Success", data);
-			let tbody = document.querySelector("#data");
-			//tbody.innerHTML="";
-			let row = document.createElement("tr");
-			// obteniendo data de la response
-			for (const [key, value] of Object.entries(data)) {
-				let elemento = key == "id_persona" ? "th" : "td";
-				let td = document.createElement(elemento);
-				//se adiciona el valor de la clave, del objeto, al td
-				td.innerHTML = `${value}`;
-				row.append(td);
-			}
-			// adicionando los botones update y delete
-			let colAcciones = document.createElement("td");
-			colAcciones.innerHTML =
-				'<button type="button" class="btn btn-success"><i class="bi bi-arrow-repeat"></i></button><button type="button" class="btn btn-danger"><i class="bi bi-trash"></i></button>';
-			row.appendChild(colAcciones);
-			tbody.append(row);
-		})
-	)
+	.then((data) => {
+		//console.log("Success", data);
+		let tbody = document.querySelector("#data");
+		for (let registro of data) {
+			tbody.innerHTML += `
+         <tr>
+         <th class="text-center">${registro.id_persona}</th>
+         <td class="text-center">${registro.nombres}</td>
+         <td class="text-center">${registro.apellidos}</td>
+         <td class="text-center">${registro.genero}</td>
+         <td class="text-center">
+         <button class="btn btn-primary btn-sm" onclick="editar(${registro.id_persona})">Editar</button>
+         <button class="btn btn-danger btn-sm" onclick="eliminar(${registro.id_persona})">eliminar</button>
+         </td>
+         </tr>
+         `;
+		}
+	})
 	.catch((err) => console.log(`error is: ${err}`));
