4b4ef3a52a7bd3e5c0d11c44f0ff772fc12fc703
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-25 22:53:25 -0500

    construccion script delete

--- js/fetchDelete.js
+++ js/fetchDelete.js
@@ -2,13 +2,41 @@ const eliminar = (id) => {
 	let url = "../../controller/controller.php?op=eliminar";
 	var data = new FormData();
 	data.append("id", id);
-
-	fetch(url, {
-		method: "post",
-		body: data,
-	})
-		.then((data) => data.text())
-		.then((text) => {
-			console.log(text);
-		});
+	Swal.fire({
+		title: "¿Esta seguro de eliminar esta persona?",
+		text: "Este cambio no podra ser revertido!",
+		icon: "warning",
+		showDenyButton: true,
+		confirmButtonText: "Eliminar",
+		confirmButtonColor: "#d33",
+		denyButtonText: "Cancelar",
+		denyButtonColor: "#aaa",
+		showLoaderOnConfirm: true,
+		preConfirm: () => {
+			return (
+				fetch(url, {
+					method: "post",
+					body: data,
+				})
+					//.then((response) => response.json())
+					.then((response) => {
+						if (!response.ok) {
+							throw new Error(response.statusText);
+						}
+						return response.json();
+					})
+					.catch((error) => {
+						Swal.showValidationMessage(`Error en Solicitud: ${error}`);
+					})
+			);
+		},
+	}).then((result) => {
+		if (result.isConfirmed) {
+			console.log("Delete reg:", result);
+			dibujarTabla(result.value);
+			Swal.fire("Registro eliminado con exito", "", "error");
+		} else if (result.isDenied) {
+			Swal.fire("Los Cambios no fueron guardados", "", "info");
+		}
+	});
 };
