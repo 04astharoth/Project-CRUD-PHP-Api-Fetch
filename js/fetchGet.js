2255628ceedc65d95cfb3f9334e2e802cd3f60f1
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-30 23:42:03 -0500

    se ajusta script de listar resultados, cuando la promesa no devuelve resultados

--- js/fetchGet.js
+++ js/fetchGet.js
@@ -3,6 +3,16 @@ fetch("../../controller/controller.php?op=listar")
 	//.then(text => console.log(text))
 	.then((data) => {
 		//console.log("Success", data);
-		dibujarTabla(data);
+		if (data.length > 0) {
+			dibujarTabla(data);
+		} else {
+			let tbody = document.querySelector("#data");
+			tbody.innerHTML += `
+            <tr>
+            <th class="text-center" colspan="5" >No hay Registros en la Base de datos.</th>
+            </td>
+            </tr>
+            `;
+		}
 	})
 	.catch((err) => console.log(`error is: ${err}`));
