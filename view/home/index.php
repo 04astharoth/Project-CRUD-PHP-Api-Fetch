3c8f9aceb0a79f6dd4c4aaadd9b73935a871e452
Jose Gil Varon Moreno <jgvm91@gmail.com>
2022-08-31 21:58:17 -0500

    se agrega vista home y titulo a la app

--- view/home/index.php
+++ view/home/index.php
@@ -5,12 +5,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>PHP WEB APP MVC</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 </head>
 
 <body>
+	<!-- Navegacion -->
+	<center>
+		<br>
+		<a href="../../">Home</a>
+	</center>
 	<!-- Formulario de datos -->
 	<div class="container mt-3">
 		<form action="" id="formdata">
